9e42ba5a8176756ad944247ebd2bcd6538063029
HritikRao30 <raohg19.extc@coep.ac.in>
2024-01-14 18:48:42 +0530

    deployed the auth app as well test 2

--- packages/auth/config/webpack.prod.js
+++ packages/auth/config/webpack.prod.js
@@ -8,7 +8,7 @@ const prodConfig = {
     mode: "production",
     output: {
         filename: '[name].[contenthash].js',
-        pubicPath : `${domain}/auth/latest/`,
+        publicPath : `${domain}/auth/latest/`,
     },
     plugins: [
         new ModuleFederationPlugin({
