0f97ab9d9e5bcd6664e227f2a38d5e52d0f5c40b
HritikRao30 <raohg19.extc@coep.ac.in>
2024-01-14 16:44:39 +0530

    public path added to prod config publicPath

--- packages/container2/config/webpack.prod.js
+++ packages/container2/config/webpack.prod.js
@@ -11,7 +11,7 @@ const prodConfig = {
     mode: "production",
     output: {
         filename: '[name].[contenthash].js',
-        publicpath: '/container/latest/'
+        publicPath: '/container/latest/'
     },
     plugins: [
         new ModuleFederationPlugin({
